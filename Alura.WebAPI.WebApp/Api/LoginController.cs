51ebbf6f0be4371f99a03433946b1d73500f97cd
LucasDev13 <lucaspontes140@gmail.com>
2021-10-28 10:32:15 -0300

    Implementado a autenticação com Jwt.

---
+++ Alura.WebAPI.WebApp/Api/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using Alura.ListaLeitura.Seguranca;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public LoginController(SignInManager<Usuario> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Token(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginModel.Login, loginModel.Password, true, true);
+                if (result.Succeeded)
+                {
+                    //criar o token - a string -> (header + payload(claim(direitors)) + signature)
+                    var direitos = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub, loginModel.Login),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    };
+
+                    var chave = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("alura-webapi-authentication-valid"));
+                    var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+                    //gerando o token
+                    var token = new JwtSecurityToken(
+                        issuer: "Alura.WebApp",
+                        audience: "Postman",
+                        claims: direitos,
+                        signingCredentials: credenciais,
+                        expires: DateTime.Now.AddMinutes(30)
+                        );
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                    return Ok(tokenString);
+                }
+                return Unauthorized();//401
+            }
+
+            return BadRequest(); //400
+        }
+    }
+}
