fb3bf0211b622e3995da40412d3bad0b34a07708
LucasDev13 <lucaspontes140@gmail.com>
2021-10-28 11:39:57 -0300

    Separado a api do aplicação mvc usando microsserviços.

---
+++ Alura.WebAPI.Api/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Alura.ListaLeitura.Api.Formatters;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LeituraContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("ListaLeitura"));
+            });
+
+            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
+
+            services.AddTransient<IRepository<Livro>, RepositorioBaseEF<Livro>>();
+
+            services.AddMvc(options => {
+                options.OutputFormatters.Add(new LivroCsvFormatter());
+            }).AddXmlSerializerFormatters();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
